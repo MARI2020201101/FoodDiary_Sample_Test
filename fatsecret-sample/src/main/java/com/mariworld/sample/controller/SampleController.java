141ef208e136c8ca84e1d2d3901acc5e9646b918
MARI <mari20201101@gmail.com>
2021-06-11 10:33:59 +0900

    Fix project build settings

--- fatsecret-sample/src/main/java/com/mariworld/sample/controller/SampleController.java
+++ fatsecret-sample/src/main/java/com/mariworld/sample/controller/SampleController.java
@@ -18,7 +18,6 @@ import com.mariworld.sample.mapper.FoodMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
